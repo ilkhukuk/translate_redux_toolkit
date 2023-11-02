45420479613c22bbbcd5973ee193747104723a0d
ilkhukuk <ilkhukuk@gmail.com>
2023-11-02 23:26:55 +0300

    first

---
+++ README.md
@@ -0,0 +1,17 @@
+# TRANSLATE APP
+
+## USED
+
+- @reduxjs/toolkit
+- react-redux
+- redux 
+- axios
+- react
+- select
+
+# Source
+https://rapidapi.com/dickyagustin/api/text-translator2
+
+## CONTACT
+
+ilkhukuk@gmail.com
---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const options = {
+  method: "GET",
+  url: "https://text-translator2.p.rapidapi.com/getLanguages",
+  headers: {
+    "X-RapidAPI-Key": "0005a96ebbmsh2267e5bb46d38a8p1c332cjsn24a70b0d0cc",
+    "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+  },
+};
